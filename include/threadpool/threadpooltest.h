d371c54479025aa90efa3ce95bb440e082f75a4e
vacantlot <1073193643@qq.com>
2025-04-15 15:50:19 +0800

    threadpool-test

---
+++ include/threadpool/threadpooltest.h
@@ -0,0 +1,31 @@
+//
+// Created by Vacant lot on 2025/4/15.
+//
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <algorithm>
+
+#include "../blockingqueue/blockingqueue.tpp"
+#include "threadpool.h"
+#include "rejecthandle.h"
+using namespace std;
+void inline threadpooltest() {
+    auto bq = make_unique<BlockQueue<std::function<void()> > >(6);
+    ThrowRejectHandle *rh = new ThrowRejectHandle();
+
+    auto mpool = make_unique<MyThreadPool>(4, 10, std::chrono::milliseconds(10),
+                                           std::move(bq), rh);
+
+    for (int i = 0; i < 10; ++i) {
+        try {
+            mpool->execute(
+                std::bind([i]() {
+                    std::cout << "Task " << i << " on thread " << std::this_thread::get_id() << std::endl;
+                }));
+        } catch (const std::exception &e) {
+            cout << e.what() << endl;
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,7 @@
 #include <vector>
 #include <thread>
 #include <algorithm>
-
-#include "../include/blockingqueue/blockingqueue.tpp"
-#include "../include/threadpool/threadpool.h"
-#include "../include/threadpool/rejecthandle.h"
 using namespace std;
 int main() {
-    auto bq = make_unique<BlockQueue<std::function<void()> > >(6);
-    ThrowRejectHandle *rh = new ThrowRejectHandle();
-
-    auto mpool = make_unique<MyThreadPool>(4, 10, std::chrono::milliseconds(10),
-                                           std::move(bq), rh);
-
-    for (int i = 0; i < 10; ++i) {
-        try {
-            mpool->execute(
-                std::bind([i]() {
-                    std::cout << "Task " << i << " on thread " << std::this_thread::get_id() << std::endl;
-                }));
-        } catch (const std::exception &e) {
-            cout << e.what() << endl;
-        }
-    }
 
 }
